437c54f4c4f3f07fe1d773914efff26d51021074
Kavi <kavivanauken@gmail.com>
2026-03-19 16:23:25 -0700

    Fixed bugs.

--- main.sh
+++ main.sh
@@ -5,7 +5,7 @@ sleep 0.5
 chmod +x init.sh code.sh deps.sh compile.sh pack.sh clean.sh update.sh
 
 read -p "Are you looking to update aseprite, or compile(c/u): " uorc
-if [ uorc = c ]; then
+if [ "$uorc" = c ]; then
     ./init.sh
     ./code.sh
     ./deps.sh
