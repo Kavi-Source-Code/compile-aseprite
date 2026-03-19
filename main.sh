3a7454247c6ebfabd197da0f1c4c9c4e0b658e3e
Kavi <kavivanauken@gmail.com>
2026-03-19 16:19:38 -0700

    Added update feature.

--- main.sh
+++ main.sh
@@ -2,12 +2,18 @@
 
 echo "Adding run permissions..."
 sleep 0.5
-chmod +x init.sh code.sh deps.sh compile.sh pack.sh clean.sh
+chmod +x init.sh code.sh deps.sh compile.sh pack.sh clean.sh update.sh
 
-./init.sh
-./code.sh
-./deps.sh
-./compile.sh
-./pack.sh
-./clean.sh
+read -p "Are you looking to update aseprite, or compile(c/u): " uorc
+if [ uorc = c ]; then
+    ./init.sh
+    ./code.sh
+    ./deps.sh
+    ./compile.sh
+    ./pack.sh
+    ./clean.sh
+
+else
+    ./update.sh
+fi
 exit 0
---
+++ update.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+cd aseprite || exit 1
+./pack.sh
+git pull
+echo "Welcome to the Aseprite updating script!"
+echo -e "\n For nontechnical (casual) users."
+sleep 0.5
+
+SKIA_DIR="$HOME/deps/skia"
+
+echo "Verifying skia dir..."
+echo "Skia dir: $SKIA_DIR"
+
+if [ -d "$SKIA_DIR" ]; then
+    sleep 2
+    echo "Compiling Aseprite"
+    sleep 2
+    mkdir -p build
+    cd build || exit 1
+    
+    echo "Configuring..."
+    sleep 2
+    read -n 1 -s -r -p "Press any key to continue"
+    sleep 1
+    
+    cmake .. \
+      -G Ninja \
+      -DCMAKE_BUILD_TYPE=RelWithDebInfo \
+      -DLAF_BACKEND=skia \
+      -DSKIA_DIR="$SKIA_DIR" \
+      -DSKIA_LIBRARY_DIR="$SKIA_DIR/out/Release-arm64" \
+      -DSKIA_LIBRARY="$SKIA_DIR/out/Release-arm64/libskia.a" \
+      -DWEBP_LIBRARIES="$SKIA_DIR/out/Release-arm64/libwebp.a" \
+      -DPNG_ARM_NEON:STRING=on
+
+    echo "Building..."
+    sleep 2
+    read -n 1 -s -r -p "Press any key to continue"
+    
+    ninja aseprite
+    echo "Congratulations! Aseprite compilation complete."
+    echo -e "- success\n:)"
+    cd bin
+    echo "Moving aseprite..."
+    mv Aseprite.app /Applications/
+    echo "..."
+    sleep 2
+    echo "Moved Aseprite."
+else
+    echo "Error: Skia directory not found at $SKIA_DIR"
+    exit 1
+fi
+
+exit 0
+
